73fed540aa5d69307a7952890ecd956060b5220e
hieu <hieuschooler@gmail.com>
2025-06-04 15:54:54 +0700

    sửa lại data

--- backend/src/main/java/org/example/doantn/Repository/CtdtRepo.java
+++ backend/src/main/java/org/example/doantn/Repository/CtdtRepo.java
@@ -4,12 +4,14 @@ import org.example.doantn.Entity.Batch;
 import org.example.doantn.Entity.Ctdt;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface CtdtRepo extends JpaRepository<Ctdt, Integer> {
-    Optional<Ctdt> findByMaCt(String maCt);
     @Query("SELECT c FROM Course c JOIN c.ctdts t WHERE t.maCt = :maCt")
+    Optional<Ctdt> findByMaCt(String maCt);
+
     Optional<Ctdt> findByName(String name);
     Optional<Ctdt> findByMaCtAndBatch(String maCt, Batch batch); // Thêm phương thức này
 
