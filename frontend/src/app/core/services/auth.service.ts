e1a0a18a9c97c2da5d2042d1b6c041b34df88989
hieu <hieuschooler@gmail.com>
2025-06-09 20:16:35 +0700

    Add missing frontend directories and files and fix import paths

---
+++ frontend/src/app/core/services/auth.service.ts
@@ -0,0 +1,39 @@
+// src/app/core/services/auth.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:8080/api/auth/login';
+
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post(this.apiUrl, { username, password });
+  }
+
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  getRole(): string | null {
+    return localStorage.getItem('role');
+  }
+
+  isLoggedIn(): boolean {
+    return !!this.getToken();
+  }
+
+  logout(): void {
+    localStorage.removeItem('token');
+    localStorage.removeItem('role');
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.getToken();
+  }
+}
